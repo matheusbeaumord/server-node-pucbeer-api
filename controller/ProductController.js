c771514015bfff4961f4d9bcdc3ddac7f82802e0
Matheus <matheusbeaumord@gmail.com>
2023-07-01 09:38:43 -0300

    fix: products

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -4,8 +4,10 @@ const ProductService = require('../service/ProductService');
 const getAllProducts = async (req, res) => {
   try {
     const result = await ProductService.getAll();
-    const allProducts = result[0].map(({ url_image, ...product }) => (
-      { ...product, urlImage: url.parse(url_image).href }));
+    const allProducts = result.map(({ url_image, ...product }) => ({
+      ...product,
+      urlImage: url.parse(url_image).href,
+    }));
     return res.status(200).json(allProducts);
   } catch (error) {
     return res.status(500).json({ error: error.message });
--- model/ProductModel.js
+++ model/ProductModel.js
@@ -2,8 +2,8 @@ const connection = require('./connection');
 
 const allProducts = async () => {
   try {
-    const products = await connection.query('SELECT * FROM products;');
-    return products;
+    const products = await connection.query('SELECT * FROM products');
+    return products.rows;
   } catch (error) {
     throw new Error('Erro de conexão');
   }
